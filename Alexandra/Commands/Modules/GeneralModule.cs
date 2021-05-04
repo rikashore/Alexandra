4d532915712de8e69fa778216b1a31f6555e0fa3
Aarav Navani <aarav.navani@outlook.com>
2021-05-04 21:28:53 +0530

    Added Files

---
+++ Alexandra/Commands/Modules/GeneralModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Disqord.Bot;
+using Disqord.Rest;
+using Qmmands;
+
+namespace Alexandra.Commands.Modules
+{
+    public class GeneralModule : DiscordModuleBase
+    {
+        [Command("ping")]
+        [Description("Plays a quick game of Ping Pong.")]
+        public async Task PingAsync()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var msg = await Response("Pong: *loading* response time");
+            stopwatch.Stop();
+
+            await msg.ModifyAsync(x => x.Content = $"Pong: {stopwatch.ElapsedMilliseconds}ms response time");
+        }
+    }
+}
---
+++ Alexandra/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Disqord.Bot.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Alexandra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    x.AddCommandLine(args);
+                    x.AddJsonFile("config.json");
+                })
+                .ConfigureLogging(x =>
+                {
+                    var logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        .WriteTo.Console(theme: SystemConsoleTheme.Grayscale)
+                        .CreateLogger();
+                    x.AddSerilog(logger, true);
+
+                    x.Services.Remove(x.Services.First(x => x.ServiceType == typeof(ILogger<>)));
+                    x.Services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+                })
+                .ConfigureDiscordBot((context, bot) =>
+                {
+                    bot.Token = context.Configuration["token"];
+                    bot.UseMentionPrefix = true;
+                    bot.Prefixes = new[] {"lex"};
+                })
+                .ConfigureServices(x =>
+                {
+                    x.AddSingleton<HttpClient>();
+                })
+                .Build();
+
+            try
+            {
+                host.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+    }
+}
