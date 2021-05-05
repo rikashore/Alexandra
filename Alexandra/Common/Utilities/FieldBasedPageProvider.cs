2b52cb1eac91474245d572277ecd0b56abc05d9c
Aarav Navani <aarav.navani@outlook.com>
2021-05-05 13:39:39 +0530

    Added field paginators

---
+++ Alexandra/Common/Utilities/FieldBasedPageProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Extensions.Interactivity.Menus.Paged;
+using Alexandra.Common.Globals;
+
+namespace Alexandra.Common.Utilities
+{
+    public class FieldBasedPageProvider : IPageProvider
+    {
+        private readonly List<Page> _pages;
+
+        private readonly FieldBasedPageProviderConfiguration _configuration;
+
+        public int PageCount { get; }
+
+        public FieldBasedPageProvider(IEnumerable<LocalEmbedFieldBuilder> fields, FieldBasedPageProviderConfiguration configuration = null)
+        {
+            _configuration = configuration ?? FieldBasedPageProviderConfiguration.Default;
+            _pages = new List<Page>();
+
+            var fieldArray = fields as LocalEmbedFieldBuilder[] ?? fields.ToArray();
+            var totalFields = fieldArray.Length;
+            // this is ugly, but ambiguous calls and math.ceiling returning a double forced my hand
+            PageCount = (int)Math.Ceiling((decimal)totalFields / _configuration.FieldsPerPage);
+            
+            CreatePages(fieldArray, _configuration.FieldsPerPage);
+        }
+
+        private void CreatePages(LocalEmbedFieldBuilder[] fields, int fieldsPerPage)
+        {
+            var loopLimit = PageCount * fieldsPerPage;
+            for (var i = 0; i < loopLimit; i+= fieldsPerPage)
+            {
+                var embedBuilder = new LocalEmbedBuilder
+                {
+                    Fields = fields[i..Math.Min(i + fieldsPerPage, fields.Length)],
+                    Color = LexGlobals.LexColor
+                };
+
+                if (_configuration.AutoGeneratePageTitles)
+                    embedBuilder.WithTitle($"Page {_pages.Count + 1}/{PageCount}");
+
+                _pages.Add(new Page(_configuration.Content, embedBuilder));
+            }
+        }
+
+        public ValueTask<Page> GetPageAsync(PagedMenu menu)
+            => new(_pages[menu.CurrentPageIndex]);
+        
+    }
+}
