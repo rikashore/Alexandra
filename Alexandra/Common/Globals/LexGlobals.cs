9e224dbec365cdb307ae6842491f2a76e4571880
Aarav Navani <aarav.navani@outlook.com>
2021-05-05 13:41:23 +0530

    Added globals

---
+++ Alexandra/Common/Globals/LexGlobals.cs
@@ -0,0 +1,24 @@
+﻿using Disqord;
+
+namespace Alexandra.Common.Globals
+{
+    public static class LexGlobals
+    {
+        public static readonly Color LexColor = new Color(0x4B2638);
+        public const string LexRepo = "https://github.com/shift-eleven/Alexandra";
+        public const ulong AuthorId = 714821968825810945;
+
+        public const string LexAscii = @"
+ ▄▄▄       ██▓    ▓█████ ▒██   ██▒ ▄▄▄       ███▄    █ ▓█████▄  ██▀███   ▄▄▄      
+▒████▄    ▓██▒    ▓█   ▀ ▒▒ █ █ ▒░▒████▄     ██ ▀█   █ ▒██▀ ██▌▓██ ▒ ██▒▒████▄    
+▒██  ▀█▄  ▒██░    ▒███   ░░  █   ░▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌▓██ ░▄█ ▒▒██  ▀█▄  
+░██▄▄▄▄██ ▒██░    ▒▓█  ▄  ░ █ █ ▒ ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌▒██▀▀█▄  ░██▄▄▄▄██ 
+ ▓█   ▓██▒░██████▒░▒████▒▒██▒ ▒██▒ ▓█   ▓██▒▒██░   ▓██░░▒████▓ ░██▓ ▒██▒ ▓█   ▓██▒
+ ▒▒   ▓▒█░░ ▒░▓  ░░░ ▒░ ░▒▒ ░ ░▓ ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░
+  ▒   ▒▒ ░░ ░ ▒  ░ ░ ░  ░░░   ░▒ ░  ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒   ░▒ ░ ▒░  ▒   ▒▒ ░
+  ░   ▒     ░ ░      ░    ░    ░    ░   ▒      ░   ░ ░  ░ ░  ░   ░░   ░   ░   ▒   
+      ░  ░    ░  ░   ░  ░ ░    ░        ░  ░         ░    ░       ░           ░  ░
+                                                        ░                         
+";
+    }
+}
