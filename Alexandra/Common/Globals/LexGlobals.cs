2da8d5cb71d9aec2193bf75d94f6971fc289768f
Aarav Navani <aarav.navani@outlook.com>
2021-05-06 15:27:11 +0530

    Added Octokit repo link

--- Alexandra/Common/Globals/LexGlobals.cs
+++ Alexandra/Common/Globals/LexGlobals.cs
@@ -6,6 +6,7 @@ namespace Alexandra.Common.Globals
     {
         public static readonly Color LexColor = new Color(0x4B2638);
         public const string LexRepo = "https://github.com/shift-eleven/Alexandra";
+        public const string OctokitRepo = "https://github.com/octokit/octokit.net";
         public const ulong AuthorId = 714821968825810945;
 
         public const string LexAscii = @"
