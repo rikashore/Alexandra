3b1eaa047afafaa849d673f3c9b5cab2045ea685
Aarav Navani <aarav.navani@outlook.com>
2021-05-06 15:26:08 +0530

    Added new extensions

---
+++ Alexandra/Common/Extensions/EmbedExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Alexandra.Common.Globals;
+using Disqord;
+
+namespace Alexandra.Common.Extensions
+{
+    public static class EmbedExtensions
+    {
+        public static LocalEmbedBuilder WithLexColor(this LocalEmbedBuilder builder)
+            => builder.WithColor(LexGlobals.LexColor);
+    }
+}
---
+++ Alexandra/Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alexandra.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static string NewlineQuote<T>(this T[] list)
+            => NewlineQuote((IEnumerable<T>)list);
+
+        public static string NewlineQuote<T>(this IEnumerable<T> list)
+            => $"> {string.Join("\n> ", list)}";
+        
+        public static T Random<T>(this T[] source, Random random = null)
+        {
+            random ??= new Random();
+            return source[random.Next(0, source.Length)];
+        }
+
+        public static T Random<T>(this IList<T> source, Random random = null)
+        {
+            random ??= new Random();
+            return source[random.Next(0, source.Count)];
+        }
+    }
+}
