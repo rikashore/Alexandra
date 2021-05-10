d91f7a856c5ce6b18f7d1d764c3e1d6e488f2e86
Aarav Navani <aarav.navani@outlook.com>
2021-05-10 21:50:45 +0530

    Added helpers

---
+++ Alexandra/Database/Helpers/NoteHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alexandra.Database.Entities;
+using Disqord;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alexandra.Database.Helpers
+{
+    public class NoteHelper
+    {
+        private readonly LexDbContext _lexDbContext;
+
+        public NoteHelper(LexDbContext lexDbContext)
+        {
+            _lexDbContext = lexDbContext;
+        }
+
+        public async Task<Note> RetrieveNoteAsync(int id)
+        {
+            var note = await _lexDbContext.Notes
+                .FindAsync(id);
+
+            return note;
+        }
+
+        public async Task<List<Note>> RetrieveNotesAsync(Snowflake ownerId)
+        {
+            var notes = await _lexDbContext.Notes.AsNoTracking()
+                .Where(x => x.OwnerId == ownerId)
+                .OrderByDescending(x => x.CreatedOn)
+                .ToListAsync();
+
+            return notes;
+        }
+
+        public async Task CreateNoteAsync(string content, Snowflake ownerId, DateTime createdOn)
+        {
+            _lexDbContext.Add(new Note {Content = content, OwnerId = ownerId, CreatedOn = createdOn});
+            await _lexDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteNoteAsync(Note note)
+        {
+            _lexDbContext.Notes.Remove(note);
+            await _lexDbContext.SaveChangesAsync();
+        }
+
+        public async Task EditNoteAsync(Note note, string content)
+        {
+            note.Content = content;
+            await _lexDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Alexandra/Database/Helpers/TagHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Alexandra.Database.Entities;
+using Disqord;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alexandra.Database.Helpers
+{
+    public class TagHelper
+    {
+        private readonly LexDbContext _lexDbContext;
+
+        public TagHelper(LexDbContext lexDbContext)
+        {
+            _lexDbContext = lexDbContext;
+        }
+
+        public async Task<Tag> RetrieveTagAsync(string tagName, Snowflake guildId)
+        {
+            var tag = await _lexDbContext.Tags
+                .FirstOrDefaultAsync(x => x.Name == tagName && x.GuildId == guildId);
+
+            return tag;
+        }
+
+        public async Task AddTagAsync(string tagName, string content, Snowflake guildId, Snowflake ownerId)
+        {
+            _lexDbContext.Add(new Tag {Name = tagName, Content = content, GuildId = guildId, OwnerId = ownerId});
+
+            await _lexDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteTagAsync(Tag tag)
+        {
+            _lexDbContext.Tags.Remove(tag);
+            await _lexDbContext.SaveChangesAsync();
+        }
+    }
+}
