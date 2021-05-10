1ed17671401077d0d75561e72e9c3afb2539178c
Aarav Navani <aarav.navani@outlook.com>
2021-05-10 21:49:39 +0530

    Added sets and converter

--- Alexandra/Database/LexDbContext.cs
+++ Alexandra/Database/LexDbContext.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Alexandra.Common.Extensions;
+using Alexandra.Database.Entities;
+using Disqord;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Alexandra.Database
 {
     public class LexDbContext : DbContext
     {
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        
         public LexDbContext(DbContextOptions<LexDbContext> options) : base(options) 
         { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var snowflakeConverter = new ValueConverter<Snowflake, ulong>(
+                static snowflake => snowflake,
+                static @ulong => new Snowflake(@ulong));
+
+            modelBuilder.UseValueConverterForType<Snowflake>(snowflakeConverter);
+        }
     }
 }
