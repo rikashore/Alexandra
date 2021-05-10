0c67aaae57c93bda9e0a700051520303814accd2
Aarav Navani <aarav.navani@outlook.com>
2021-05-10 17:23:01 +0530

    Added Code block and related parser

---
+++ Alexandra/Commands/Parsers/CodeBlockTypeParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Alexandra.Common.Types;
+using Disqord.Bot;
+using Qmmands;
+
+namespace Alexandra.Commands.Parsers
+{
+    public class CodeBlockTypeParser : DiscordTypeParser<CodeBlock>
+    {
+        public override ValueTask<TypeParserResult<CodeBlock>> ParseAsync(Parameter parameter, string value, DiscordCommandContext context)
+        {
+            var cs1 = value.IndexOf($"```", StringComparison.OrdinalIgnoreCase);
+            if (cs1 == -1)
+                return Failure("Not a valid code block");
+            
+            cs1 = value.IndexOf('\n', cs1) + 1;
+            var cs2 = value.LastIndexOf("```", StringComparison.OrdinalIgnoreCase);
+
+            if (cs1 == -1 || cs2 == -1)
+                return Failure("Not a valid code block");
+
+            var code = value.Substring(cs1, cs2 - cs1);
+            return Success(new CodeBlock(code));
+        }
+    }
+}
---
+++ Alexandra/Common/Types/CodeBlock.cs
@@ -0,0 +1,17 @@
+﻿namespace Alexandra.Common.Types
+{
+    public class CodeBlock
+    {
+        public string Text { get; set; }
+
+        public CodeBlock(string text)
+        {
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"```\n{Text}\n```";
+        }
+    }
+}
