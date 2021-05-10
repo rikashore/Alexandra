4c8e3d5a015bb3223efebb0b9ef1db741ba990fb
Aarav Navani <aarav.navani@outlook.com>
2021-05-10 21:50:08 +0530

    Added Db entities

---
+++ Alexandra/Database/Entities/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Alexandra.Common.Extensions;
+using Disqord;
+
+namespace Alexandra.Database.Entities
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public Snowflake OwnerId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; }
+        
+        public override string ToString()
+        {
+            var article = CreatedOn.Date == DateTime.Today ? "at" : "on";
+            return $"{Content} (Created {article} {CreatedOn.Humanize()})";
+        }
+    }
+}
---
+++ Alexandra/Database/Entities/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Disqord;
+
+namespace Alexandra.Database.Entities
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public Snowflake GuildId { get; set; }
+        public Snowflake OwnerId { get; set; }
+        public string Content { get; set; }
+    }
+}
