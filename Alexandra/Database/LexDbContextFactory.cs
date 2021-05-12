e9d0e27ccb87e1e5306a4b0a83a44c7d3c52e5ba
Aarav Navani <aarav.navani@outlook.com>
2021-05-12 19:36:29 +0530

    Made use of config builder

--- Alexandra/Database/LexDbContextFactory.cs
+++ Alexandra/Database/LexDbContextFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace Alexandra.Database
 {
@@ -8,9 +9,13 @@ namespace Alexandra.Database
     {
         public LexDbContext CreateDbContext(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("./config.json")
+                .Build();
+            
             var optionsBuilder = new DbContextOptionsBuilder<LexDbContext>();
             optionsBuilder.UseMySql(
-                "",
+                config["dbconn"],
                 new MySqlServerVersion(new Version(8, 0, 21)));
 
             return new LexDbContext(optionsBuilder.Options);
