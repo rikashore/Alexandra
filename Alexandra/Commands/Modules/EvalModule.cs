2297bac7161ed6797d79c9f96b1a02d23088af65
Aarav Navani <aarav.navani@outlook.com>
2021-05-14 12:49:15 +0530

    Added eval module

---
+++ Alexandra/Commands/Modules/EvalModule.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Alexandra.Common.Types;
+using Alexandra.Services;
+using Disqord;
+using Disqord.Bot;
+using Qmmands;
+
+namespace Alexandra.Commands.Modules
+{
+    [Group("eval")]
+    public class EvalModule : DiscordModuleBase
+    {
+        private readonly EvalService _evalService;
+        private readonly ParseService _parseService;
+
+        public EvalModule(EvalService evalService, ParseService parseService)
+        {
+            _evalService = evalService;
+            _parseService = parseService;
+        }
+        
+        [Command("Lua")]
+        [Description("Evaluate some Lua code")]
+        [RequireBotOwner]
+        public DiscordCommandResult EvalLuaAsync([Remainder]string codeString)
+        {
+            var parseSuccess = _parseService.TryParseCodeBlock(codeString, out CodeBlock codeBlock);
+            
+            if(parseSuccess)
+            {
+                var evalResult = _evalService.EvalLuaCode(codeBlock);
+                return Response(evalResult);
+            }
+            
+            return Response("It seems you haven't given me code to execute");
+        }
+    }
+}
