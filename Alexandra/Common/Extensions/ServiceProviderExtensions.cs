b6ce3924bdfcb2a2e602df20a6b141e14d04e8d5
Aarav Navani <aarav.navani@outlook.com>
2021-05-14 12:49:46 +0530

    Added service extensions

---
+++ Alexandra/Common/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Reflection;
+using Alexandra.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alexandra.Common.Extensions
+{
+    public static class ServiceProviderExtensions
+    {
+        public static IServiceCollection AddLexServices(this IServiceCollection services)
+        {
+            var baseType = typeof(LexService);
+            var types = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(x => x.IsAssignableTo(baseType) && !x.IsAbstract);
+
+            foreach (var type in types)
+                services.AddSingleton(type);
+
+            return services;
+        }
+    }
+}
