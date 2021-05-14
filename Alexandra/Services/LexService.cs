7ea29d9f080d2888b6d6fed33b2c11771d175a7a
Aarav Navani <aarav.navani@outlook.com>
2021-05-14 12:48:53 +0530

    Added base service type

---
+++ Alexandra/Services/LexService.cs
@@ -0,0 +1,15 @@
+﻿using Disqord.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace Alexandra.Services
+{
+    public abstract class LexService : ILogging
+    {
+        public ILogger Logger { get; }
+        
+        protected LexService(ILogger logger)
+        {
+            Logger = logger;
+        }
+    }
+}
