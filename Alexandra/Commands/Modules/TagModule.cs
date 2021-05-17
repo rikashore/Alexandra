def201050c4bf128beb496a6d5f919a408144288
Aarav Navani <aarav.navani@outlook.com>
2021-05-17 09:57:55 +0530

    Added new commands

--- Alexandra/Commands/Modules/TagModule.cs
+++ Alexandra/Commands/Modules/TagModule.cs
@@ -1,26 +1,38 @@
-﻿using System.Threading.Tasks;
-using Alexandra.Database.Helpers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alexandra.Common.Extensions;
+using Alexandra.Services;
+using Disqord;
 using Disqord.Bot;
+using Disqord.Extensions.Interactivity.Menus.Paged;
+using Disqord.Gateway;
+using Disqord.Rest;
 using Qmmands;
 
+// Most of the newer commands are from 230Daniel
+// https://github.com/230Daniel
 namespace Alexandra.Commands.Modules
 {
-    [Group("tag", "tags")]
+    [Group("tag", "tags", "t")]
     [Name("Tag")]
     [Description("Commands relating to tags")]
     public class TagModule : DiscordGuildModuleBase
     {
-        private readonly TagHelper _tagHelper;
+        private readonly TagHelper _tagService;
+        private readonly CommandService _commandService;
         
-        public TagModule(TagHelper tagHelper)
+        public TagModule(TagHelper tagService, CommandService commandService)
         {
-            _tagHelper = tagHelper;
+            _tagService = tagService;
+            _commandService = commandService;
         }
         
         [Command]
         public async Task<DiscordCommandResult> GetTagAsync(string tagName)
         {
-            var tag = await _tagHelper.RetrieveTagAsync(tagName, Context.GuildId);
+            var tag = await _tagService.RetrieveTagAsync(tagName, Context.GuildId);
 
             if (tag is null)
                 return Response("It seems no tag with that name could be found.");
@@ -28,44 +40,138 @@ namespace Alexandra.Commands.Modules
             return Response(tag.Content);
         }
 
-        [Command("add")]
+        [Command("add", "create")]
         public async Task<DiscordCommandResult> AddTagAsync(string tagName, [Remainder] string content)
         {
-            var tag = await _tagHelper.RetrieveTagAsync(tagName, Context.GuildId);
+            var tag = await _tagService.RetrieveTagAsync(tagName, Context.GuildId);
 
             if (tag is not null)
                 return Response("It seems a tag with this name already exists.");
+            if (!IsTagNameValid(tagName))
+                return Response($"The tag name \"{tagName}\" is forbidden, please choose another name.");
 
-            await _tagHelper.AddTagAsync(tagName, content, Context.GuildId, Context.Author.Id);
+            await _tagService.AddTagAsync(tagName, content, Context.GuildId, Context.Author.Id);
             return Response($"I have successfully added {tagName}.");
         }
 
         [Command("delete", "del", "remove")]
         public async Task<DiscordCommandResult> DeleteTagAsync(string tagName)
         {
-            var tag = await _tagHelper.RetrieveTagAsync(tagName, Context.GuildId);
+            var tag = await _tagService.RetrieveTagAsync(tagName, Context.GuildId);
 
             if (tag is null)
                 return Response("It seems no tag with that name could be found.");
             if (tag.OwnerId != Context.Author.Id)
                 return Response("I cannot let you delete other's tags.");
 
-            await _tagHelper.DeleteTagAsync(tag);
+            await _tagService.DeleteTagAsync(tag);
             return Response("I have successfully deleted that tag.");
         }
 
         [Command("edit")]
         public async Task<DiscordCommandResult> EditTagAsync(string tagName, [Remainder] string newContent)
         {
-            var tag = await _tagHelper.RetrieveTagAsync(tagName, Context.GuildId);
+            var tag = await _tagService.RetrieveTagAsync(tagName, Context.GuildId);
 
             if (tag is null)
                 return Response("It seems no tag with that name could be found.");
             if (tag.OwnerId != Context.Author.Id)
                 return Response("I cannot let you edit other's tags.");
 
-            await _tagHelper.EditTagAsync(tag, newContent);
+            await _tagService.EditTagAsync(tag, newContent);
             return Response("I have edited that tag successfully.");
+        }
+
+        [Command("list")]
+        public async Task<DiscordCommandResult> ListTagsAsync()
+        {
+            var tags = await _tagService.RetrieveTagsAsync(Context.GuildId);
+            tags = tags.OrderByDescending(x => x.Uses).ToList();
+
+            var i = 0;
+            var tagStrings = tags.Select(x => $"`#{++i}` {x.Name} ({Mention.User(x.OwnerId)})\n");
+            var stringPages = new List<string>();
+            
+            var current = "";
+            foreach (var tagString in tagStrings)
+            {
+                if((current + tagString).Length <= 2048)
+                    current += tagString;
+                else
+                {
+                    stringPages.Add(current);
+                    current = tagString;
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(current))
+                stringPages.Add(current);
+
+            var pages = stringPages.Select(x => new Page(
+                    new LocalEmbedBuilder()
+                        .WithLexColor()
+                        .WithTitle("Tags")
+                        .WithDescription(x)
+                        .WithFooter($"Page {stringPages.IndexOf(x) + 1} of {stringPages.Count}")))
+                .ToList();
+
+            return pages.Count switch
+            {
+                0 => Response("There are no tags for this server."),
+                1 => Response(pages[0].Embed),
+                _ => Pages(pages)
+            };
+        }
+        
+        [Command("claim")]
+        public async Task<DiscordCommandResult> Claim([Remainder] string name)
+        {
+            var tag = await _tagService.RetrieveTagAsync(name ,Context.GuildId);
+            if (tag is null) 
+                return await TagNotFoundResponse(name);
+            
+            var member = Context.Guild.GetMember(tag.OwnerId) ?? 
+                         await Context.Guild.FetchMemberAsync(tag.OwnerId);
+                
+            if (member is not null)
+                return Response($"The owner of the tag \"{name}\" is still in the server.");
+
+            tag.OwnerId = Context.Message.Author.Id;
+            await _tagService.UpdateTagAsync(tag);
+            
+            return Response($"Ownership of the tag \"{name}\" was successfully transferred to you.");
+        }
+        
+        [Command("transfer")]
+        public async Task<DiscordCommandResult> Transfer(string name, [Remainder] [RequireNotBot] IMember member)
+        {
+            var tag = await _tagService.RetrieveTagAsync(name,Context.GuildId);
+            if (tag is null) 
+                return await TagNotFoundResponse(name);
+            
+            if (tag.OwnerId != Context.Author.Id)
+                return Response($"The tag \"{name}\" does not belong to you.");
+
+            tag.OwnerId = member.Id;
+            await _tagService.UpdateTagAsync(tag);
+            
+            return Response($"Ownership of the tag \"{name}\" was successfully transferred to {member.Mention}.");
+        }
+        
+        private bool IsTagNameValid(string name)
+            => _commandService
+                .GetAllModules()
+                .First(x => x.Type == typeof(TagModule)).Commands
+                .All(x => x.Aliases
+                    .All(y => !string.Equals(y, name, StringComparison.CurrentCultureIgnoreCase)));
+        
+        private async Task<DiscordCommandResult> TagNotFoundResponse(string name)
+        {
+            var closeTags = await _tagService.SearchTagsAsync(Context.GuildId, name);
+            if (closeTags.Count == 0) 
+                return Response($"I couldn't find a tag with the name \"{name}\".");
+            
+            var didYouMean = " • " + string.Join("\n • ", closeTags.Take(3).Select(x => x.Name));
+            return Response($"It seems I couldn't find a tag with the name \"{name}\", did you mean...\n{didYouMean}");
         }
     }
 }
