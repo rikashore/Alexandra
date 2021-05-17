0a88c1ce808a1ead8be75796ad584fa986f4d43f
Aarav Navani <aarav.navani@outlook.com>
2021-05-17 09:57:46 +0530

    Refactored Tags

--- Alexandra/Database/Entities/Tag.cs
+++ Alexandra/Database/Entities/Tag.cs
@@ -5,12 +5,11 @@ namespace Alexandra.Database.Entities
 {
     public class Tag
     {
-        [Key]
-        public int Id { get; set; }
-        
         public string Name { get; set; }
         public Snowflake GuildId { get; set; }
         public Snowflake OwnerId { get; set; }
         public string Content { get; set; }
+        public uint Revisions { get; set; }
+        public uint Uses { get; set; }
     }
 }
---
+++ Alexandra/Services/TagService.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alexandra.Database;
+using Alexandra.Database.Entities;
+using Disqord;
+using Microsoft.EntityFrameworkCore;
+using MinimumEditDistance;
+
+namespace Alexandra.Services
+{
+    public class TagHelper
+    {
+        private readonly LexDbContext _lexDbContext;
+
+        public TagHelper(LexDbContext lexDbContext)
+        {
+            _lexDbContext = lexDbContext;
+        }
+
+        public async Task<Tag> RetrieveTagAsync(string tagName, Snowflake guildId)
+        {
+            var tag = await _lexDbContext.Tags
+                .FirstOrDefaultAsync(x => x.GuildId == guildId && EF.Functions.ILike(x.Name, tagName));
+
+            return tag;
+        }
+
+        public async Task<List<Tag>> RetrieveTagsAsync(Snowflake guildId)
+        {
+            return await _lexDbContext.Tags.Where(x => x.GuildId == guildId).ToListAsync();
+        }
+
+        public async Task<List<Tag>> SearchTagsAsync(Snowflake guildId, string searchQuery)
+        {
+            var tags = await RetrieveTagsAsync(guildId);
+            return tags
+                .Select(x => (Levenshtein.CalculateDistance(x.Name, searchQuery, 2), x))
+                .Where(x => x.Item1 <= 5)
+                .OrderBy(x => x.Item1)
+                .Select(x => x.Item2)
+                .ToList();
+        }
+        
+        public async Task<int> GetTagRankAsync(Tag tag)
+        {
+            var tags = await RetrieveTagsAsync(tag.GuildId);
+            return tags.OrderByDescending(x => x.Uses).TakeWhile(x => x.Name != tag.Name).Count() + 1;
+        }
+
+        public async Task AddTagAsync(string tagName, string content, Snowflake guildId, Snowflake ownerId)
+        {
+            _lexDbContext.Add(new Tag {Name = tagName, Content = content, GuildId = guildId, OwnerId = ownerId});
+
+            await _lexDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteTagAsync(Tag tag)
+        {
+            _lexDbContext.Tags.Attach(tag);
+            _lexDbContext.Tags.Remove(tag);
+            await _lexDbContext.SaveChangesAsync();
+        }
+
+        public async Task EditTagAsync(Tag tag, string newContent)
+        {
+            tag.Content = newContent;
+            tag.Revisions++;
+            await UpdateTagAsync(tag);
+        }
+
+        public async Task UpdateTagAsync(Tag tag)
+        {
+            _lexDbContext.Tags.Update(tag);
+            await _lexDbContext.SaveChangesAsync();
+        }
+    }
+}
