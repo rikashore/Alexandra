6490aaeff911e1540d5ba395b39ac81fd6881079
Aarav Navani <aarav.navani@outlook.com>
2021-05-17 10:10:00 +0530

    Added new Migrations

---
+++ Alexandra/Database/Migrations/20210517043850_SecondVersion.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Alexandra.Database.Migrations
+{
+    public partial class SecondVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_tags",
+                table: "tags");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "tags");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "tags",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "revisions",
+                table: "tags",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "uses",
+                table: "tags",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_tags",
+                table: "tags",
+                columns: new[] { "name", "guild_id" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_tags",
+                table: "tags");
+
+            migrationBuilder.DropColumn(
+                name: "revisions",
+                table: "tags");
+
+            migrationBuilder.DropColumn(
+                name: "uses",
+                table: "tags");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "tags",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "tags",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_tags",
+                table: "tags",
+                column: "id");
+        }
+    }
+}
