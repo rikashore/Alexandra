7da0dec084fe8a642053f1f831abe102c5a69b90
Aarav Navani <aarav.navani@outlook.com>
2021-05-22 19:02:08 +0530

    Added LexModuleBase

---
+++ Alexandra/Commands/Bases/LexModuleBase.cs
@@ -0,0 +1,16 @@
+﻿using Disqord.Bot;
+
+namespace Alexandra.Commands.Bases
+{
+    public class LexModuleBase : DiscordModuleBase
+    {
+        protected DiscordCommandResult NoteNotFoundResponse()
+            => Response("No note with that ID could be found");
+
+        protected DiscordCommandResult InvalidAccessResponse()
+            => Response("I cannot let you edit other's notes");
+
+        protected DiscordCommandResult InvalidCodeResponse()
+            => Response("It seems you haven't given me code to execute");
+    }
+}
