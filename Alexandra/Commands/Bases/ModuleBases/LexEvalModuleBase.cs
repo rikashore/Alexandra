b5fb5a7b94ed5042a0964b01956f0b78cdfcf91d
Aarav Navani <aarav.navani@outlook.com>
2021-05-28 21:56:58 +0530

    Removed Parse service

--- Alexandra/Commands/Bases/ModuleBases/LexEvalModuleBase.cs
+++ Alexandra/Commands/Bases/ModuleBases/LexEvalModuleBase.cs
@@ -7,8 +7,7 @@ namespace Alexandra.Commands.Bases.ModuleBases
     public class LexEvalModuleBase : LexModuleBase
     {
         protected EvalService EvalService => Context.Services.GetRequiredService<EvalService>();
-        protected ParseService ParseService => Context.Services.GetRequiredService<ParseService>();
-        
+
         protected DiscordCommandResult InvalidCodeResponse()
             => Response("It seems you haven't given me code to execute");
     }
