361766a26ccc3be5f44bdf110bb571af93f88c15
Aarav Navani <aarav.navani@outlook.com>
2021-05-31 18:36:18 +0530

    Simplified class

--- Alexandra/Common/Types/ColorInfoData.cs
+++ Alexandra/Common/Types/ColorInfoData.cs
@@ -11,70 +11,16 @@ namespace Alexandra.Common.Types
         public string Clean { get; set; }
     }
 
-    public class FractionRgb
-    {
-        [JsonProperty("r")]
-        public double R { get; set; }
-        
-        [JsonProperty("g")]
-        public double G { get; set; }
-        
-        [JsonProperty("b")]
-        public double B { get; set; }
-    }
-
-    public class FractionHsl
-    {
-        [JsonProperty("h")]
-        public double H { get; set; }
-
-        [JsonProperty("s")]
-        public double S { get; set; }
-
-        [JsonProperty("l")]
-        public double L { get; set; }
-    }
-
-    public class FractionHsv
-    {
-        [JsonProperty("h")]
-        public double H { get; set; }
-
-        [JsonProperty("s")]
-        public double S { get; set; }
-        
-        [JsonProperty("v")]
-        public double V { get; set; }
-    }
-
-    public class FractionCmyk
-    {
-        [JsonProperty("c")]
-        public double C { get; set; }
-
-        [JsonProperty("m")]
-        public double M { get; set; }
-
-        [JsonProperty("y")]
-        public double Y { get; set; }
-
-        [JsonProperty("k")]
-        public double K { get; set; }
-    }
-
     public class Rgb
     {
-        [JsonProperty("fraction")]
-        public FractionRgb Fraction { get; set; }
-
-        [JsonProperty("r")]
-        public int R { get; set; }
-
-        [JsonProperty("g")]
-        public int G { get; set; }
-
-        [JsonProperty("b")]
-        public int B { get; set; }
+        // [JsonProperty("r")]
+        // public int R { get; set; }
+        //
+        // [JsonProperty("g")]
+        // public int G { get; set; }
+        //
+        // [JsonProperty("b")]
+        // public int B { get; set; }
 
         [JsonProperty("value")]
         public string Value { get; set; }
@@ -82,17 +28,14 @@ namespace Alexandra.Common.Types
 
     public class Hsl
     {
-        [JsonProperty("fraction")]
-        public FractionHsl Fraction { get; set; }
-
-        [JsonProperty("h")]
-        public int H { get; set; }
-
-        [JsonProperty("s")]
-        public int S { get; set; }
-
-        [JsonProperty("l")]
-        public int L { get; set; }
+        // [JsonProperty("h")]
+        // public int H { get; set; }
+        //
+        // [JsonProperty("s")]
+        // public int S { get; set; }
+        //
+        // [JsonProperty("l")]
+        // public int L { get; set; }
 
         [JsonProperty("value")]
         public string Value { get; set; }
@@ -100,20 +43,17 @@ namespace Alexandra.Common.Types
 
     public class Hsv
     {
-        [JsonProperty("fraction")]
-        public FractionHsv Fraction { get; set; }
-
-        [JsonProperty("h")]
-        public int H { get; set; }
-
-        [JsonProperty("s")]
-        public int S { get; set; }
-
+        // [JsonProperty("h")]
+        // public int H { get; set; }
+        //
+        // [JsonProperty("s")]
+        // public int S { get; set; }
+        //
+        // [JsonProperty("v")]
+        // public int V { get; set; }
+        
         [JsonProperty("value")]
         public string Value { get; set; }
-
-        [JsonProperty("v")]
-        public int V { get; set; }
     }
 
     public class Name
@@ -133,38 +73,20 @@ namespace Alexandra.Common.Types
 
     public class Cmyk
     {
-        [JsonProperty("fraction")]
-        public FractionCmyk Fraction { get; set; }
-
         [JsonProperty("value")]
         public string Value { get; set; }
 
-        [JsonProperty("c")]
-        public int C { get; set; }
-
-        [JsonProperty("m")]
-        public int M { get; set; }
-
-        [JsonProperty("y")]
-        public int Y { get; set; }
-
-        [JsonProperty("k")]
-        public int K { get; set; }
-    }
-
-    public class Image
-    {
-        [JsonProperty("bare")]
-        public string Bare { get; set; }
-
-        [JsonProperty("named")]
-        public string Named { get; set; }
-    }
-
-    public class Contrast
-    {
-        [JsonProperty("value")]
-        public string Value { get; set; }
+        // [JsonProperty("c")]
+        // public int C { get; set; }
+        //
+        // [JsonProperty("m")]
+        // public int M { get; set; }
+        //
+        // [JsonProperty("y")]
+        // public int Y { get; set; }
+        //
+        // [JsonProperty("k")]
+        // public int K { get; set; }
     }
 
     public class ColorInfoData
@@ -186,11 +108,5 @@ namespace Alexandra.Common.Types
 
         [JsonProperty("cmyk")]
         public Cmyk Cmyk { get; set; }
-
-        [JsonProperty("image")]
-        public Image Image { get; set; }
-
-        [JsonProperty("contrast")]
-        public Contrast Contrast { get; set; }
     }
 }
