0a7d92b086e05e59787f39ab578f8072926ab3f5
Aarav Navani <aarav.navani@outlook.com>
2021-07-05 14:03:10 +0530

    Added PastyListPageProvider

---
+++ Alexandra/Common/Utilities/PastyListPageProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Disqord.Extensions.Interactivity.Menus.Paged;
+
+namespace Alexandra.Common.Utilities
+{
+    public class PastyListPageProvider : ListPageProvider
+    {
+        public PastyListPageProvider(IEnumerable<Page> pages) : base(pages)
+        { }
+
+        public PastyListPageProvider(List<Page> pages) : base(pages)
+        { }
+    }
+}
