807b1c4db7030bec58b0f640f2a7a742f4f67cf0
Aarav Navani <aarav.navani@outlook.com>
2021-07-05 14:02:40 +0530

    Added MystPasteService

---
+++ Alexandra/Services/MystPasteService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MystPaste.NET;
+
+namespace Alexandra.Services
+{
+    public class MystPasteService : LexService
+    {
+        private readonly MystPasteClient _mystPasteClient;
+
+        public MystPasteService(ILogger<MystPasteService> logger) : base(logger)
+        {
+            _mystPasteClient = new MystPasteClient();
+        }
+
+        public async Task<List<Pasty>> GetPasties(string id)
+        {
+            var paste = await _mystPasteClient.Paste.GetPasteAsync(id);
+            return paste.Pasties;
+        }
+
+        public Task<Language> GetLanguageByNameAsync(string language)
+        {
+            return _mystPasteClient.Data.GetLanguageByNameAsync(language);
+        }
+
+        public Task<Language> GetLanguageByExtensionAsync(string extension)
+        {
+            return _mystPasteClient.Data.GetLanguageByExtensionAsync(extension);
+        }
+    }
+}
